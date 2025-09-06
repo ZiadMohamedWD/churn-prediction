818a38befde21937f7b85b938665eea0a03ceb9c
ZiadMohamedWD <ziadm162@gmail.com>
2025-09-06 03:20:33 +0300

    Add files via upload

--- STAPP.py
+++ STAPP.py
@@ -1,12 +1,10 @@
 import streamlit as st
 import numpy as np
 import joblib
-import matplotlib.pyplot as plt
 
 model = joblib.load("Gaussian_model.pk1")
 
 st.title("Customer Churn Prediction")
-
 
 age = st.slider("Age", min_value=18, max_value=100, value=30)
 tenure = st.slider("Tenure", min_value=0, max_value=50, value=5)
@@ -24,10 +22,3 @@ else:
     st.write("This customer will Not Churn.")
 
 st.write(f"Probability of Churn: {probability:.2%}")
-
-fig, ax = plt.subplots(figsize=(4, 1.2))
-ax.barh(["Churn", "Not Churn"], [probability, 1 - probability], color=["red", "green"])
-ax.set_xlim(0, 1)
-for i, v in enumerate([probability, 1 - probability]):
-    ax.text(v + 0.02, i, f"{v:.1%}", va="center")
-st.pyplot(fig)
